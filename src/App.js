25c671cb3e728ce8b439498fd9969bff1d8c20a6
Darius Loftis <dariusloftis@dariuss-mbp.attlocal.net>
2019-11-18 14:18:26 -0800

    updated clock component, created currentTime component, styles for grid, styles for clock component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Clock from './components/Clock';
+import CurrentTime from './components/CurrentTime'
 
 function App() {
   return (
     <div className="App">
-      <Clock />
+      <div className="container">
+        <div className="row">
+          <Clock />
+          <CurrentTime />
+        </div>
+      </div>
     </div>
   );
 }
