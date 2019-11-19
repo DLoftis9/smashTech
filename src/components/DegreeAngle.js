bd8551a9c8fd5248b9103c4c77f6787304fb3591
Darius Loftis <dariusloftis@dariuss-mbp.attlocal.net>
2019-11-18 19:47:22 -0800

    logic for DegreeAngle, removed unused html markup

--- src/components/DegreeAngle.js
+++ src/components/DegreeAngle.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 const propTypes = {
     minute: PropTypes.number,
     hour: PropTypes.number,
+    time: PropTypes.objectOf(PropTypes.func).isRequired,
 };
 
 const defaultProps = {
-    minute: 15,
-    hour: 2,
+    minute: 38,
+    hour: 7,
+    time: new Date(),
 };
+
+function hourTime(time) {
+    return time.getHours() % 12 || 12;
+}
+
+function minuteTime(time) {
+    return time.getMinutes();
+}
 
 function DegreeAngle (props) {
 
-    var minuteHand = 360 * (props.minute/60);
-    var hourHand = (360 * (props.hour/12)) + ((360/12) * (props.minute/60));
-    
-    console.log(minuteHand - hourHand);
+    // var minuteHand = 360 * (props.minute/60);
+    // var hourHand = (360 * (props.hour/12)) + ((360/12) * (props.minute/60));
+
+    var minuteHand = 360 * (minuteTime(props.time)/60);
+    var hourHand = (360 * (hourTime(props.time)/12)) + ((360/12) * (minuteTime(props.time)/60));
     
     return (
-        
         <React.Fragment>
-            <p>I'm a test {minuteHand} and {hourHand}</p>
+            <h3>I'm a test {minuteHand} and {hourHand}</h3>
             
-            <p>{minuteHand > hourHand ? (
-                <span className="first-condition">{minuteHand - hourHand}</span>
+            <div>{minuteHand > hourHand ? (
+                <React.Fragment>
+                    <p className="first-condition">The current degrees for the angle of the clock hands: {minuteHand - hourHand}</p>
+                    <p>{hourTime(props.time)}</p>
+                    <p>{minuteTime(props.time)}</p>
+                </React.Fragment>
             ) : (
-                <span className="second-condition">{hourHand - minuteHand}</span>
-            )}</p>
+                <React.Fragment>
+                    <p className="second-condition">{hourHand - minuteHand}</p>
+                    <p>{hourTime(props.time)}</p>
+                    <p>{minuteTime(props.time)}</p>
+                </React.Fragment>
+            )}</div>
         </React.Fragment>
     );
 }
@@ -37,23 +54,3 @@ export default DegreeAngle;
 
 DegreeAngle.propTypes = propTypes;
 DegreeAngle.defaultProps = defaultProps;
-
-// function clock_angles(hour, minute) {
-//     // first workout the minute angle
-//       var minAngle = 360*(minute/60);
-//     // then work out hour angle
-//     // first the hour then the hour + mins
-//       var hourAngle = (360*(hour/12))+((360/12)*(minute/60));
-//       console.log("minute angle = " + minAngle);
-//       console.log("hour angle = " + hourAngle);
-//     //if minute angle greater than hour angle
-//       if(minAngle > hourAngle) {
-//           return minAngle - hourAngle;
-//       }
-//     //if minute angle less than minute angle
-//       else {
-//           return hourAngle - minAngle;
-//       }
-//   }
-  
-//   console.log(clock_angles(2,15));
