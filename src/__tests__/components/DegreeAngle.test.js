8ccc88afa07a5736e9c40153f06d93e19dee499e
Darius Loftis <dariusloftis@dariuss-mbp.attlocal.net>
2019-11-19 15:35:41 -0800

    created tests for app

--- src/__tests__/components/DegreeAngle.test.js
+++ src/__tests__/components/DegreeAngle.test.js
@@ -5,7 +5,6 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 import DegreeAngle from '../../components/DegreeAngle';
-import App from '../../App';
 
 describe('DegreeAngle', () => {
   it('renders without crashing', () => {
